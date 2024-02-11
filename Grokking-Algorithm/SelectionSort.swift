9247bc8b3ff5035908709b557d6a94d57400e78a
Mr.Bakai <ismailov.time@gmail.com>
2024-02-11 14:28:58 +0600

    Initial Commit

---
+++ Grokking-Algorithm/SelectionSort.swift
@@ -0,0 +1,36 @@
+//
+//  SelectionSort.swift
+//  Grokking-Algorithm
+//
+//  Created by Bakai Ismaiilov on 11/2/24.
+//
+
+import Foundation
+
+// MARK: - Some kind of Selection Sort
+/// sorts an array from smallest to biggest
+func selectionSort(_ arr: [Int]) -> [Int] {
+    var newArr: [Int] = []
+    var mutableArray = arr
+    
+    for _ in 0..<arr.count {
+        guard let smallestIndex = findSmallest(mutableArray) else { continue }
+        newArr.append(mutableArray.remove(at: smallestIndex))
+    }
+    
+    return newArr
+}
+
+private func findSmallest(_ arr: [Int]) -> Int? {
+    guard !arr.isEmpty else { return nil }
+    var smallest = 0
+    var smallestIndex = 0
+    
+    for i in 0..<arr.count {
+        if arr[i] < smallest {
+            smallest = arr[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
---
+++ Grokking-Algorithm/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  Grokking-Algorithm
+//
+//  Created by Bakai Ismaiilov on 11/2/24.
+//
+
+import Foundation
+
+// MARK: - Binary Search
+/*
+ Binary search only works when your list is in sorted order.
+*/
+
+private func binarySearch(_ list: [Int], item: Int) -> Int? {
+    var low = 0
+    var high = (list.count - 1)     /// `low` and `high` keep track of which part of the list you’ll search in.
+    
+    while low <= high {             /// While you haven’t narrowed it down to one element ...
+        var mid = (low + high) / 2
+        var guess = list[mid]
+        
+        if guess == item {          /// Found the item.
+            return mid
+        } else if (guess > item){   /// The guess was too high.
+            high = mid - 1
+        } else {
+            low = mid + 1           /// The guess was too low.
+        }
+    }
+    
+    return nil                      /// The item doesn’t exist.
+}
+
+print("BINARY SEARCH: \(binarySearch([1,2,3,4,5], item: 4))")
+
+print("SELCTION SORT: \(selectionSort([1,2,3,4,5]))")
