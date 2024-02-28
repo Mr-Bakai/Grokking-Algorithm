829be25973c83ed9b0c81b088937dd1f4ca4501b
Mr.Bakai <ismailov.time@gmail.com>
2024-02-28 21:15:53 +0600

    GA: graphB is updated on DijkstrasAlgorithm

--- Grokking-Algorithm/DijkstrasAlgorithm.swift
+++ Grokking-Algorithm/DijkstrasAlgorithm.swift
@@ -183,14 +183,15 @@ func getGraphB() -> (
     var proccessed: [String]
     
     // MARK: Graph
-    graph["start"] = ["a": 6, "b": 2]
-    graph["a"] = ["fin": 1]
-    graph["b"] = ["a": 3, "fin": 5]
+    graph["start"] = ["a": 10]
+    graph["a"] = ["b": 20]
+    graph["b"] = ["c": 1, "fin": 30]
+    graph["c"] = ["a": 1]
     graph["fin"] = [:]
     
     // MARK: Costs
-    costs["a"] = 6
-    costs["b"] = 2
+    costs["a"] = 10
+    costs["b"] = infinity
     costs["c"] = infinity
     costs["fin"] = infinity
     
